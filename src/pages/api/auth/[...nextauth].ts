0fcd9c388c6f07a15767aaa10c79b388b5994da6
GirogiMakharadze <makharadzegiorgi00@gmail.com>
2023-02-13 19:48:48 +0400

    connect to Mondgodb & NextAuth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
