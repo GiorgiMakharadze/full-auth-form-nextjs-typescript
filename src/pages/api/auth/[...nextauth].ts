45945f304bc04481f688e747b6e87a92d71f77aa
GirogiMakharadze <makharadzegiorgi00@gmail.com>
2023-02-14 00:36:03 +0400

    added Mongodb adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,8 +5,11 @@ import GoogleProvider from "next-auth/providers/google";
 import DiscordProvider from "next-auth/providers/discord";
 import TwitterProvider from "next-auth/providers/twitter";
 import Auth0Provider from "next-auth/providers/auth0";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
@@ -31,7 +34,7 @@ export const authOptions = {
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID as string,
       clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
-      issuer: process.env.AUTH0_ISSUER,
+      issuer: process.env.AUTH0_ISSUER as string,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
