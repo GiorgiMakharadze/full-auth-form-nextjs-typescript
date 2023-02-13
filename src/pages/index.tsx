b531b9c07a9f7715fd6fbfb544d724a9ca48f847
GirogiMakharadze <makharadzegiorgi00@gmail.com>
2023-02-13 20:51:26 +0400

    added providers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,22 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-red-700 text-4xl">Wlcome to course</h1>
+      <h1 className="text-red-700 text-4xl bg-yellow-400">Wlcome to course</h1>
+      {session?.user?.name}
+      <img
+        src={session?.user?.image!}
+        alt="img"
+        className="w-32 h-32 rounded-full"
+      />
+      {session ? (
+        <button onClick={() => signOut()} className="bg-blue-500">
+          signOut
+        </button>
+      ) : (
+        <button onClick={() => signIn()} className="bg-blue-500">
+          signIn
+        </button>
+      )}
     </>
   );
 }
